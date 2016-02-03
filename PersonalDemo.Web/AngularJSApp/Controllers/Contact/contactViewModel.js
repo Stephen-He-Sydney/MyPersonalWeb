d997c167759007d9496f830a84f6d57fe6856897
purplestar1912 <australianstephenhe@gmail.com>
2016-02-03 11:08:04 +0800

    Add a draft contact form.

--- PersonalDemo.Web/AngularJSApp/Controllers/Contact/contactViewModel.js
+++ PersonalDemo.Web/AngularJSApp/Controllers/Contact/contactViewModel.js
@@ -1,5 +1,13 @@
-﻿appMainModule.controller("contactViewModel", ["$rootScope", function ($rootScope) {
+﻿appMainModule.controller("contactViewModel", ["$scope", "$rootScope", function ($scope, $rootScope) {
 
     $rootScope.$broadcast("changeNavBar", { status: "contact" });
     // emit would let parent scope only hear it
+
+    $scope.submitForm = function (isValid) {
+
+        if (isValid) {
+            alert('Valid!!!');
+        }
+    };
+
 }]);
